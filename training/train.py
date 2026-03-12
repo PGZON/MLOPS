819e311dabd0732bbc7e998c3973590c82f111d3
pgzon <prathameshgadgil2005@gmail.com>
2026-03-12 12:08:21 +0530

    remove unused Docker and Kubernetes files; update training workflow and model training script

--- training/train.py
+++ training/train.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import LinearRegression
 
 from huggingface_hub import HfApi
 
+# Load dataset
 df = pd.read_csv("data/CleanedData.csv")
 
 X = df[['name','company','year','kms_driven','fuel_type']]
@@ -17,24 +18,38 @@ y = df['Price']
 categorical = ['name','company','fuel_type']
 numeric = ['year','kms_driven']
 
+# Preprocessing
 preprocessor = ColumnTransformer([
     ('cat', OneHotEncoder(handle_unknown="ignore"), categorical),
     ('num', 'passthrough', numeric)
 ])
 
+# Pipeline
 pipeline = Pipeline([
     ('preprocessor', preprocessor),
     ('model', LinearRegression())
 ])
 
-pipeline.fit(X,y)
+# Train model
+pipeline.fit(X, y)
 
+# Save model
 os.makedirs("model", exist_ok=True)
 
 model_path = "model/car_price_pipeline.pkl"
 
-pickle.dump(pipeline, open(model_path,"wb"))
+with open(model_path, "wb") as f:
+    pickle.dump(pipeline, f)
 
+print("Model saved locally")
+
+# Read token
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+if HF_TOKEN is None:
+    raise ValueError("HF_TOKEN not found. Set it as environment variable.")
+
+# Upload model
 api = HfApi()
 
 api.upload_file(
@@ -42,7 +57,7 @@ api.upload_file(
     path_in_repo="car_price_pipeline.pkl",
     repo_id="PG05/car-price-model",
     repo_type="model",
-    token=os.environ["HF_TOKEN"]
+    token=HF_TOKEN
 )
 
 print("Model uploaded to HuggingFace")
